75d0157c8d32f95831634427158f10d0783f0e02
7984686717 <parthmadhad04@gmail.com>
2024-06-04 18:39:56 +0530

    add file

---
+++ Module 2.mjs
@@ -0,0 +1,9 @@
+const a = "Parth";
+const b = "Prince";
+const c = "Dhaval";
+const d = "Dhruv";
+
+export default a;
+export {b};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 
 import './App.css';
-import img1 from './Image/01.jpg';
-let name="Parth";
+
 
 function App() {
   return (
     <>
-    <header>
-      <h1>{name}</h1>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Context Us</li>
-        </ul>
-      </nav>
-
-      <p className="lorem-p">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error similique doloribus corrupti laborum voluptate qui iure libero et, voluptatibus eligendi tempore culpa illo consequatur repudiandae magnam reprehenderit voluptas dignissimos neque.
-      </p>
-
-      <img src={img1} alt="img1" width="100%"/>
-    </header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SIT</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 }

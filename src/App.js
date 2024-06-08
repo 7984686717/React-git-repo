d3bf68bffbdcaca963683b5084e203255dcc1477
7984686717 <parthmadhad04@gmail.com>
2024-06-08 17:14:44 +0530

    add Navbae.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = 'black'
+    } else {
+      setMode('light')
+      document.body.style.background = 'white'
+    }
+  }
   return (
     <>
-      <Navbar title="SIT" aboutText="Parth"></Navbar>
+      <Navbar title="SIT" aboutText="Parth" mode={mode} toggleMode={toggleMode}></Navbar>
       <TextForm heading="Intexr Text to Analyse Below"></TextForm>
     </>
   );

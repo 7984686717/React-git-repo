971c84faa6c83d88dbb787dd7960e19f61a87090
7984686717 <parthmadhad04@gmail.com>
2024-06-08 16:50:14 +0530

    add About us file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <>
       <Navbar title="SIT" aboutText="Parth"></Navbar>
-      <TextForm heading="Intexr Text to Analyse Below"></TextForm>
+      {/* <TextForm heading="Intexr Text to Analyse Below"></TextForm> */}
+      <AboutUs heading="About Us"></AboutUs>
     </>
   );
 }

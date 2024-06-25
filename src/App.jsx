2981d5247664856aa256e2f3df07fef70afd4119
7984686717 <parthmadhad04@gmail.com>
2024-06-25 16:19:08 +0530

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import Privacypolicy from './components/privacyPolicy';
+import { creacteBowserRouter, RouterPrivider } from 'react-router-dom';
 
 
 
 function App() {
 
-
-  const [mode, setMode] = useState('light'); 
+  
+  const [mode, setMode] = useState('light');
 
   const [btnText, newbtnText] = useState('Enable Dark Mode');
 
-  const [alert, setAlert] = useState (null);
+  const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
@@ -31,15 +33,15 @@ function App() {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.background = 'black';
-      document.body.style.color= 'white';
+      document.body.style.color = 'white';
       newbtnText('Enable to Dark Mode');
       showAlert("Dark Mode Has Been Enable", "sucess")
 
       setTimeout(() => {
-        document.title= "SIT - Admission";
+        document.title = "SIT - Admission";
       }, 1000);
       setTimeout(() => {
-        document.title= "SIT - B.sc.IT"
+        document.title = "SIT - B.sc.IT"
       }, 500);
     } else {
       setMode('light')
@@ -49,11 +51,31 @@ function App() {
       showAlert("Light Mode Has Been Enable", "sucess")
     }
   }
+
+  const router = creacteBowserRouter([{
+    path: '/Home',
+    element: <>
+      <Navbar title="SIT" aboutText="Prince" mode={mode} toggleMode={toggleMode} btntext={btnText}></Navbar>
+      <Alert alert={alert} />
+      <TextForm heading="Intexr Text to Analyse Below" mode={mode} toggleMode={toggleMode} showAlert={showAlert}></TextForm>
+    </>
+  },
+
+ {
+    path: '/Privacypolicy',
+    element: <>
+      <Navbar title="SIT" aboutText="Prince" mode={mode} toggleMode={toggleMode} btntext={btnText}></Navbar>
+      <Privacypolicy heading="Privacy Policy"></Privacypolicy>
+      <TextForm heading="Intexr Text to Analyse Below" mode={mode} toggleMode={toggleMode} showAlert={showAlert}></TextForm>
+    </>
+  }
+  ])
+
+
   return (
     <>
-      <Navbar title="SIT" aboutText="Prince" mode={mode} toggleMode={toggleMode} btntext={btnText}></Navbar>
-      <Alert alert={alert}/>
-      <TextForm heading="Intexr Text to Analyse Below" mode={mode} toggleMode={toggleMode} showAlert={showAlert}></TextForm>
+
+    <RouterPrivider router={router}/>
     </>
   );
 }
